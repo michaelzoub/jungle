0af0d22e375bd241ac6caccfff6fc8416315081c
1mints <micacao15@gmail.com>
2025-05-03 15:48:03 -0400

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['placehold.co', 'via.placeholder.com', 'picsum.photos', '25bdafa6284dfabfef0d3bdb29c20ba4.r2.cloudflarestorage.com', 'pub-59a6ccbae6794ea6be0f116149d286eb.r2.dev'],
+    domains: ['placehold.co', 'via.placeholder.com', 'picsum.photos', '25bdafa6284dfabfef0d3bdb29c20ba4.r2.cloudflarestorage.com', 'pub-59a6ccbae6794ea6be0f116149d286eb.r2.dev', 'r2-worker.micacao15.workers.dev'],
   },
 }
 
