ecf22d0239893b829bf53312aeb579e06cc48556
1mints <micacao15@gmail.com>
2025-05-07 17:51:30 -0400

    added employee login + review page

--- src/app/review/[slug]/page.tsx
+++ src/app/review/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import ReviewForm from "@/app/review/[slug]/ReviewForm"
+interface Props {
+    params: {
+      slug: string;
+    };
+  }
 
-export default async function ReviewPage({
-    params,
-}: {
-    params: { slug: string }
-}) {
+
+export default async function ReviewPage({ params }: Props) {
     return <ReviewForm slug={params.slug} />
 }
